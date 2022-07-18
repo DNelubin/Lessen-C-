46617107900831a8bd4a9e5bb8516811498ac000
Dmitriy <NelubinDO@mail.ru>
2022-07-18 21:15:53 +0900

    Add rootFolder Example_005_Giri and change file

---
+++ Example_005_Giri/Program.cs
@@ -0,0 +1,39 @@
+﻿int a = 3;
+int b = 8;
+int c = 5;
+int d = 7;
+int i = 9;
+int max = a;
+if (b>a)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+if (c>b)
+{
+    max = c;
+}
+else
+{
+    max = b;
+}
+if (d>c)
+{
+    max = d;
+}
+else
+{
+    max = c;
+}
+if (i>d)
+{
+    max = i;
+}
+else
+{
+    max = d;
+}
+Console.Write (max);
